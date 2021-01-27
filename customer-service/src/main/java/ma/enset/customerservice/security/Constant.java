7bcee805e4589ace2c7cfa9d62e7687671004277
Taoufiq-Imad <Taoufiq-Imad@users.noreply.github.com>
2021-01-27 10:38:18 +0100

    add security and client

--- customer-service/src/main/java/ma/enset/customerservice/security/Constant.java
+++
@@ -1,7 +0,0 @@
-package ma.enset.customerservice.security;
-
-public class Constant {
-    public static final String HEADER="Authorization";
-    public static final String KEY="imadglsid2021";
-    public static final String STARTWITH="glsidenset ";
-}
